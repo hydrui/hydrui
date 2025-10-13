f83a30330e0fa69cf10f4b15783e7fbfe363f30b
John Chadwick <john@jchw.io>
2025-10-12 23:21:12 -0400

    Add -no-auth for server mode without -htpasswd

---
+++ web/hydrui-client/src/components/screens/NoAuthScreen/NoAuthScreen.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+
+import { client, useApiActions, verifyAuthentication } from "@/store/apiStore";
+
+import "./index.css";
+
+let globalLogin: Promise<void> | undefined = undefined;
+
+const NoAuthScreen: React.FC = () => {
+  const { setAuthenticated } = useApiActions();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function logIn() {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch("/login", {
+          method: "POST",
+          body: JSON.stringify({ username: "", password: "" }),
+        });
+        if (response.status !== 200) {
+          setError("Failed to create a session.");
+          return;
+        }
+        if (await verifyAuthentication(client, "", "")) {
+          setAuthenticated(true);
+        } else {
+          setError(
+            "Hydrus API key is invalid. Check Hydrui server configuration.",
+          );
+        }
+      } catch (err) {
+        setError("Failed to connect to Hydrus API.");
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+        globalLogin = undefined;
+      }
+    }
+    if (!globalLogin) {
+      globalLogin = logIn();
+    }
+  }, [setAuthenticated]);
+
+  return (
+    <div className="no-auth-screen">
+      <div className="no-auth-form-container">
+        <h1 className="no-auth-heading">Start Session</h1>
+        {isLoading && <p className="no-auth-message">Creating session...</p>}
+        {error && <div className="no-auth-error">{error}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default NoAuthScreen;
--- web/hydrui-client/src/utils/serverMode.ts
+++ web/hydrui-client/src/utils/serverMode.ts
@@ -1,2 +1,3 @@
 export const isServerMode =
   document.body.getAttribute("data-server-mode") === "true";
+export const noAuth = document.body.getAttribute("data-no-auth") === "true";
