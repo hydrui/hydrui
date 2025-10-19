dc4efb0a68969308baf2fbe6696e226ee841e276
John Chadwick <john@jchw.io>
2025-10-19 18:21:03 -0400

    Fix blob URL leak in merged PSD renderer

--- web/hydrui-client/src/file/psd/PSDViewerMerged.tsx
+++ web/hydrui-client/src/file/psd/PSDViewerMerged.tsx
@@ -17,9 +17,22 @@ export function MergedPSDViewer({
   const [mergedUrl, setMergedUrl] = useState<string>();
   useEffect(() => {
     const render = async () => {
-      setMergedUrl(String(await renderMergedImage(String(fileUrl))));
+      const url = String(await renderMergedImage(String(fileUrl)));
+      setMergedUrl(url);
+      return url;
     };
-    render();
+    const promise = render();
+    return () => {
+      promise.then((url) => {
+        // In React strict mode this will probably cause some spurious errors
+        // This is because it will render once, unmount, then mount again.
+        // This happens so quickly that it is likely to force the blob URL to
+        // get revoked once before the DOM has a chance to update. But, it's
+        // kind of okay, because it will get replaced with a working blob URL
+        // anyways.
+        URL.revokeObjectURL(url);
+      });
+    };
   }, [fileUrl]);
   if (!mergedUrl) {
     return <div>Loading PSD...</div>;
